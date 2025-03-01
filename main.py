47e7c198ee3beb53b41fffac313a029fb66f72eb
Daniel17n <daniel16n@hotmail.com>
2025-03-01 19:46:10 +0100

    Code revised by Daniel, not a clue whats going on

--- main.py
+++ main.py
@@ -22,12 +22,9 @@ def set_device ():
 
 def load_samples (directory, size=None):
     data = pd.read_csv(directory, sep='\t', header=None) 
-    
     X = []
     Y = []
     for i in range(len(data)):
-        # print('.', end='')
-        # carga la imagen
         imgname = data.iloc[i,0]
         img = cv2.imread(imgname)
         if size != None:
@@ -37,118 +34,47 @@ def load_samples (directory, size=None):
 
     return (np.array(X), np.array(Y))
 
-'''
-# class ConvolutionalNN1(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-# 
-#         self.layers = nn.Sequential(
-#             nn.Conv2d(3, 8, kernel_size=3, stride=1),
-#             nn.BatchNorm2d(8),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
-# 
-#             nn.LazyConv2d(16, kernel_size=3, stride=1),
-#             nn.BatchNorm2d(16),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
-# 
-#             # nn.LazyConv2d(16, kernel_size=3, stride=1),
-#             # nn.BatchNorm2d(16),
-#             # nn.ReLU(),
-#             # nn.MaxPool2d(2, 2),
-# 
-#             nn.Flatten()
-#         )
-# 
-#         # Capa densa (fully connected)
-#         self.fc = nn.Sequential(
-#             nn.LazyLinear(256),
-#             nn.LazyLinear(1)
-#         )
-# 
-#     def forward(self, x):
-#         x = self.layers(x)
-#         x = self.fc(x)
-#         return x
-#     
-# class ConvolutionalNN2(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-# 
-#         self.layers = nn.Sequential(
-#             nn.Conv2d(3, 8, kernel_size=3, stride=1),
-#             nn.BatchNorm2d(8),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
-# 
-#             nn.LazyConv2d(16, kernel_size=3, stride=1),
-#             nn.BatchNorm2d(16),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
-# 
-#             nn.LazyConv2d(16, kernel_size=3, stride=1),
-#             nn.BatchNorm2d(16),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
-# 
-#             nn.Flatten()
-#         )
-# 
-#         # Capa densa (fully connected)
-#         self.fc = nn.Sequential(
-#             nn.LazyLinear(256),
-#             nn.LazyLinear(1)
-#         )
-# 
-#     def forward(self, x):
-#         x = self.layers(x)
-#         x = self.fc(x)
-#         return x
-'''
-
 class ConvolutionalNN3(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.layers = nn.Sequential(
-            nn.Conv2d(3, out_channels=16, kernel_size=3, padding='same'),
-            nn.Conv2d(16, out_channels=32, kernel_size=3, padding='same'),
+            # 64 x 64
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding='same'),
+            nn.BatchNorm2d(16),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding='same'),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # 32 x 32
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding='same'),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-            nn.LazyConv2d(out_channels=64, kernel_size=3, padding='same'),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-
-           # nn.LazyConv2d(out_channels=16, kernel_size=3, stride=1),
-           # nn.BatchNorm2d(16),
-           # nn.ReLU(),
-           # nn.MaxPool2d(2, 2),
-
+            # 16 x 16
             nn.Flatten()
         )
 
-        # Capa densa (fully connected)
         self.fc = nn.Sequential(
-            nn.LazyLinear(256),
+            nn.Linear(32 * 16 * 16, 128),  # Num_features * ancho_img * alto_img
             nn.ReLU(),
-            nn.LazyLinear(1),
+            # nn.Dropout(0.3),
+            nn.Linear(128, 1),
             nn.Sigmoid()
         )
 
     def forward(self, x):
-        x = self.layers(x)
-        x = self.fc(x)
-        return x
+        x_layers = self.layers(x)
+        output = self.fc(x_layers)
+        return output
 
-def custom_min_mse_loss(input: torch.Tensor, target: torch.Tensor):
-    diff = input - target
-    min_diff = torch.min(diff, 1 - diff)
-    loss = min_diff ** 2
-    return loss.mean()
+def custom_loss(y_pred, y_true):
+    diff = y_pred - y_true
+    # actual_diff = torch.where(diff > 0.5, 1 - diff, diff)
+    loss = torch.mean(diff ** 2) # Mean Square Error de manual
+    return loss
 
 def train(X_images, Y_angles, model, loss_fn, optimizer, epochs=1000, device='cuda', trace=100, label="Model"):
     model = model.to(device)
@@ -162,51 +88,52 @@ def train(X_images, Y_angles, model, loss_fn, optimizer, epochs=1000, device='cu
         optimizer.zero_grad()  # Reset gradients
         pred = model(X_gpu)  # Forward pass
         loss = loss_fn(pred, Y_gpu)  # Compute loss
-
         loss.backward()  # Backpropagation
         optimizer.step()  # Update parameters
-
         if (epoch + 1) % trace == 0:  # Store loss every `trace` epochs
-            loss_value = math.sqrt(loss.item())  # Root square of loss
+            loss_value = math.sqrt(loss)  # Root square of loss
             current = epoch + 1
             epochs_list.append(current)
             loss_list.append(loss_value)
 
-            print(f"{label} | loss_sqrt: {loss_value:>7f} | [{current:>5d}/{epochs:>5d}]")
+            print(f"loss_sqrt: {loss_value:>7f} | [{current:>5d}/{epochs:>5d}]")
 
     return epochs_list, loss_list
 
-def plotGraph(X, Y, model, optimizer, device, label, color):
-    epochs, loss = train(X.to(torch.float32).permute(0,3,1,2), Y.to(torch.float32), model.to(torch.float32), custom_min_mse_loss, optimizer, epochs=200, device=device, trace=1, label=label)
+def train_and_graph(X, Y, model, optimizer, device, color, label="Model"):
+    epochs, loss = train(
+        X.to(torch.float32).permute(0,3,1,2), 
+        Y.to(torch.float32), 
+        model=model.to(torch.float32), 
+        loss_fn=custom_loss, 
+        optimizer=optimizer, 
+        epochs=200, 
+        device=device, 
+        trace=1, 
+        label=label)
+
     plt.plot(epochs, loss, linestyle='-', linewidth=2, color=color, label=label)
     min_loss, min_epoch = min(loss), epochs[np.argmin(loss)]
     plt.scatter(min_epoch, min_loss, color=color, marker='o', label=f"Min {label}: {min_loss:.4f} at epoch {min_epoch}")
 
 def main():
-    display(HTML("<style>.container { width:100% !important; }</style>"))
-    X, Y = load_samples("dataset_cleaned.csv",(64,64))
+    # display(HTML("<style>.container { width:100% !important; }</style>"))
+    X, Y = load_samples("dataset_cleaned.csv", (64,64) )
     print(f"\nX shape: {X.shape}")
     print(f"\nY shape: {Y.shape}")
     
-    # get train and test sets
-    X_rest, X_test, Y_rest, Y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.20, random_state=42) # this random_state is similar to the bootstrap results
+    # get train, validation and test sets
+    X_rest, X_test, Y_rest, Y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.10, random_state=42) # this random_state is similar to the bootstrap results
     X_train, X_validation, Y_train, Y_validation = sklearn.model_selection.train_test_split(X_rest, Y_rest, test_size=0.10, random_state=42) # this random_state is similar to the bootstrap results
     
     device = set_device()
-    X_train_torch = torch.from_numpy(X_train)
-    Y_train_torch = torch.from_numpy(Y_train)
-    # model1 = ConvolutionalNN1()
-    # model2 = ConvolutionalNN2()
+    X_train_torch = torch.from_numpy(X_train).float()
+    Y_train_torch = torch.from_numpy(Y_train).float()
     model3 = ConvolutionalNN3()
-    # optimizer1 = torch.optim.Adam(model1.parameters())
-    # optimizer2 = torch.optim.Adam(model2.parameters())
-    optimizer3 = torch.optim.Adam(model3.parameters())
-
+    optimizer3 = torch.optim.Adam(model3.parameters(), lr=1e-3)
     plt.figure(figsize=(12, 8), dpi=300)
 
-    # plotGraph(X_train, Y_train, model1, optimizer1, device, "2 Layer Model", 'r')
-    # plotGraph(X_train, Y_train, model2, optimizer2, device, "3 Layer Model", 'g')
-    plotGraph(X_train_torch, Y_train_torch, model3, optimizer3, device, "4 Layer Model", 'b')
+    train_and_graph(X_train_torch, Y_train_torch, model3, optimizer3, device, 'b')
 
     # Etiquetas y formato
     plt.xlabel("Epochs", fontsize=14)
